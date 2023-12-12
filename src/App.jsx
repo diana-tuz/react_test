ce8ce848b2985069a50a6f7d9ed019972037de47
Diana Tuz <dianatuz12@gmail.com>
2023-12-12 21:34:01 +0200

    add example for useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import { GoodsTable } from "./components/GoodsTable";
 import { goods } from "./goods";
 
-
 function App() {
-  const filteredGoods = goods.filter((good) => good.type === 'fruits')
+  const filteredGoods = goods.filter((good) => good.type === "fruits");
   // const sortedGoods = filteredGoods.sort((good1, good2) => good1.price - good2.price)
-  const sortedGoods = filteredGoods.sort((good1, good2) => good1.name.localeCompare(good2.name))
-  
+  const sortedGoods = filteredGoods.sort((good1, good2) =>
+    good1.name.localeCompare(good2.name)
+  );
+
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
-      <GoodsTable goods={sortedGoods}/>
+      <button type="button" onClick={() => setNumber(number + 1)}>
+        Pressed {number}
+      </button>
+      <GoodsTable goods={sortedGoods} />
     </div>
   );
 }
